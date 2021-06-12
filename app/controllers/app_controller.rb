d9bc0a4bbd31145a8bd5c379fe89a9b62c78b9e9
Uziel <uzielluna24@gmail.com>
2021-06-12 10:32:40 -0300

    Otro avance de app

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,4 +1,7 @@
 class AppController < ApplicationController
     def index
+        @guardarropas= Guardarropa.page params[:page]
+        @atuendos=Atuendo.page params[:page]
+        @prendas = Prenda.page params[:page]
     end
 end
